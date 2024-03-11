98ec6e371b181dca0af1e1c1387931f50b75eece
midCenturyDude7 <mgriffes@gmail.com>
2024-03-10 19:40:48 -0700

    Finished up project

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,5 +1,14 @@
 "use client";
+import Link from "next/link";
 import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import {
+  FiChevronDown,
+  FiArrowRight,
+  FiBarChart2,
+  FiHome,
+  FiPieChart,
+} from "react-icons/fi";
 
 export default function Menu() {
   return (
@@ -10,24 +19,266 @@ export default function Menu() {
 }
 
 const Tabs = () => {
-  return <div></div>;
+  const [selected, setSelected] = React.useState<number | null>(1);
+  const [dir, setDir] = React.useState<null | "l" | "r">(null);
+  const handleSetSelected = (val: number | null) => {
+    if (typeof selected === "number" && typeof val === "number") {
+      setDir(selected > val ? "r" : "l");
+    } else if (val === null) {
+      setDir(null);
+    }
+
+    setSelected(val);
+  };
+
+  return (
+    <div
+      onMouseLeave={() => handleSetSelected(null)}
+      className="relative flex h-fit gap-2"
+    >
+      {TABS.map((t) => {
+        return (
+          <Tab
+            key={t.id}
+            selected={selected}
+            handleSetSelected={handleSetSelected}
+            tab={t.id}
+          >
+            {t.title}
+          </Tab>
+        );
+      })}
+
+      <AnimatePresence>
+        {selected && <Content dir={dir} selected={selected} />}
+      </AnimatePresence>
+    </div>
+  );
 };
 
-const ExampleComponent = () => {
-  return <div>Hello, world</div>;
+const Tab = ({
+  children,
+  tab,
+  handleSetSelected,
+  selected,
+}: {
+  children: React.ReactNode;
+  tab: number;
+  handleSetSelected: (val: number | null) => void;
+  selected: number | null;
+}) => {
+  return (
+    <button
+      id={`shift-tab-${tab}`}
+      onMouseEnter={() => handleSetSelected(tab)}
+      onClick={() => handleSetSelected(tab)}
+      className={`flex items-center gap-1 rounded-full px-3 py-1.5 text-sm transition-colors ${
+        selected === tab
+          ? "bg-neutral-800 text-neutral-100"
+          : "text-neutral-400"
+      }`}
+    >
+      <span>{children}</span>
+      <FiChevronDown
+        className={`transition-transform ${
+          selected === tab ? "rotate-180" : ""
+        }`}
+      />
+    </button>
+  );
 };
+
+const Content = ({
+  selected,
+  dir,
+}: {
+  selected: number | null;
+  dir: null | "l" | "r";
+}) => {
+  return (
+    <motion.div
+      id="overlay-content"
+      initial={{
+        opacity: 1,
+        y: 0,
+      }}
+      exit={{
+        opacity: 0,
+        y: 8,
+      }}
+      className="absolute left-0 top-[calc(100%_+_24px)] w-96 rounded-lg border border-neutral-600 bg-gradient-to-b from-neutral-900 via-neutral-900 to-neutral-800 p-4"
+    >
+      <Bridge />
+      <Nub selected={selected} />
+
+      {TABS.map((t) => {
+        return (
+          <div className="overflow-hidden" key={t.id}>
+            {selected === t.id && (
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  x: dir === "l" ? 100 : dir === "r" ? -100 : 0,
+                }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.25, ease: "easeInOut" }}
+              >
+                <t.Component />
+              </motion.div>
+            )}
+          </div>
+        );
+      })}
+    </motion.div>
+  );
+};
+
+const Nub = ({ selected }: { selected: number | null }) => {
+  const [left, setLeft] = React.useState(0);
+
+  React.useEffect(() => {
+    moveNub();
+  }, [selected]);
+
+  const moveNub = () => {
+    if (selected) {
+      const hoveredTab = document.getElementById(`shift-tab-${selected}`);
+      const overlayContent = document.getElementById("overlay-content");
+
+      if (!hoveredTab || !overlayContent) return;
+
+      const tabRect = hoveredTab.getBoundingClientRect();
+      const { left: contentLeft } = overlayContent.getBoundingClientRect();
+
+      const tabCenter = tabRect.left + tabRect.width / 2 - contentLeft;
+      setLeft(tabCenter);
+    }
+  };
+
+  return (
+    <motion.span
+      style={{
+        clipPath: "polygon(0 0, 100% 0, 50% 50%, 0% 100%)",
+      }}
+      animate={{ left }}
+      className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 rounded-tl border border-neutral-600 bg-neutral-900"
+    ></motion.span>
+  );
+};
+
+const Products = () => {
+  return (
+    <div>
+      <div className="flex gap-4">
+        <div>
+          <h3 className="mb-2 text-sm font-medium">Startup</h3>
+          <Link href="#" className="mb-1 block text-sm text-neutral-400">
+            Bookkeeping
+          </Link>
+          <Link href="#" className="mb-1 block text-sm text-neutral-400">
+            Invoicing
+          </Link>
+        </div>
+        <div>
+          <h3 className="mb-2 text-sm font-medium">Enterprise</h3>
+          <Link href="#" className="mb-1 block text-sm text-neutral-400">
+            White glove
+          </Link>
+          <Link href="#" className="mb-1 block text-sm text-neutral-400">
+            SOX Compliance
+          </Link>
+          <Link href="#" className="mb-1 block text-sm text-neutral-400">
+            Staffing
+          </Link>
+          <Link href="#" className="mb-1 block text-sm text-neutral-400">
+            More
+          </Link>
+        </div>
+      </div>
+
+      <button className="ml-auto mt-4 flex items-center gap-1 text-sm text-indigo-300">
+        <span>View more</span>
+        <FiArrowRight />
+      </button>
+    </div>
+  );
+};
+
+function Pricing() {
+  return (
+    <div className="grid grid-cols-3 gap-4 divide-x divide-neutral-700">
+      <Link
+        href="#"
+        className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
+      >
+        <FiHome className="mb-2 text-xl text-indigo-300" />
+        <span className="text-xs">Startup</span>
+      </Link>
+      <Link
+        href="#"
+        className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
+      >
+        <FiBarChart2 className="mb-2 text-xl text-indigo-300" />
+        <span className="text-xs">Scaleup</span>
+      </Link>
+      <Link
+        href="#"
+        className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
+      >
+        <FiPieChart className="mb-2 text-xl text-indigo-300" />
+        <span className="text-xs">Enterprise</span>
+      </Link>
+    </div>
+  );
+}
+
+const Blog = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-2">
+        <Link href="#">
+          <h4>Lorem ipsum dolor</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo
+            quidem eos.
+          </p>
+        </Link>
+        <Link href="#">
+          <h4>Lorem ipsum dolor</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo
+            quidem eos.
+          </p>
+        </Link>
+      </div>
+
+      <button className="ml-auto mt-4 flex items-center gap-1 text-sm text-indigo-300">
+        <span>View more</span>
+        <FiArrowRight />
+      </button>
+    </div>
+  );
+};
+
+const Bridge = () => (
+  <div className="absolute -top-[24px] left-0 right-0 h-[24px]"></div>
+);
+
+// const ExampleComponent = () => {
+//   return <div>Hello, world</div>;
+// };
 
 const TABS = [
   {
     title: "Products",
-    Component: ExampleComponent,
+    Component: Products,
   },
   {
     title: "Pricing",
-    Component: ExampleComponent,
+    Component: Pricing,
   },
   {
     title: "Blog",
-    Component: ExampleComponent,
+    Component: Blog,
   },
 ].map((n, idx) => ({ ...n, id: idx + 1 }));
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      Some content here...
+      Menu built with Nextjs, Tailwindcss and Framer-motion
     </div>
   );
 }
